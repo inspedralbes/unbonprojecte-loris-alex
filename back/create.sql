2106d504b6b2702c85a86a0cccb7d232754a11e0
loriscrisafo <123160689+loriscrisafo@users.noreply.github.com>
2024-03-05 09:24:52 +0100

    Creacio de fitxers sql fet TG-11 #closed"

---
+++ back/create.sql
@@ -0,0 +1,21 @@
+-- Dades
+-- Creacio de la BBDD persones amb la taula noms
+
+SET NAMES utf8;
+SET time_zone = '+00:00';
+SET foreign_key_checks = 0;
+SET sql_mode = 'NO_AUTO_VALUE_ON_ZERO';
+
+SET NAMES utf8mb4;
+DROP DATABASE IF EXISTS `persones`;
+CREATE DATABASE `persones` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `persones`;
+
+DROP TABLE IF EXISTS `noms`;
+CREATE TABLE `noms` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `nom` varchar(100) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+
